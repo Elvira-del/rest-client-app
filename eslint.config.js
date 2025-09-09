16b8e35d52f8083312572886c9fb1f02d6b833da
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2025-09-09 23:03:40 +0300

    fix: Linter config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,35 @@
+import { config } from 'typescript-eslint';
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import next from 'eslint-config-next';
 
-export default defineConfig([
-  { ignores: ['.next', 'dist', 'coverage'] },
-  ...next(['core-web-vitals']),
+export default config(
+  { ignores: ['dist', '.next', 'coverage'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  reactHooks.configs['recommended-latest'],
   {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/triple-slash-reference': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: 'module',
       globals: globals.browser,
     },
   },
-  eslintConfigPrettier,
-]);
+  eslintConfigPrettier
+);
 
 // export default tseslint.config([
 //   globalIgnores(['dist']),
