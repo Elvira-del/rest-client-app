42346715d2368a575627bce95ab11b8984d7efdf
Elvira Krutova <ninelostr@gmail.com>
2025-09-21 01:59:25 +0100

    fix: change routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'ru', 'de'],
   defaultLocale: 'en',
+  localeDetection: false,
   pathnames: {
     '/': '/',
     '/signin': {
